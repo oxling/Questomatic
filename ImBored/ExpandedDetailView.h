5b4ec136273013ce73bea8b9d296ef7501c78387
Amy Dyer <oxling@gmail.com>
2012-02-11 09:55:55 -0500

    Ignoring more files...

---
+++ ImBored/ExpandedDetailView.h
@@ -0,0 +1,13 @@
+//
+//  ExpandedDetailView.h
+//  ImBored
+//
+//  Created by Amy Dyer on 2/5/12.
+//  Copyright (c) 2012 Intuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpandedDetailView : UIView
+
+@end
---
+++ ImBored/ExpandedDetailView.m
@@ -0,0 +1,31 @@
+//
+//  ExpandedDetailView.m
+//  ImBored
+//
+//  Created by Amy Dyer on 2/5/12.
+//  Copyright (c) 2012 Intuit. All rights reserved.
+//
+
+#import "ExpandedDetailView.h"
+
+@implementation ExpandedDetailView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
